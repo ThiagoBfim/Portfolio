9dde946f011bb734bf56c08ba659300e720ce828
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-12-29 07:03:01 -0300

    Inclusão do detalhamento das atividade da timeline

--- lib/timeline/time_line_repository.dart
+++ lib/timeline/time_line_repository.dart
@@ -5,11 +5,17 @@ class TimeLine {
   final String name;
   final String iconAsset;
   final String subtitle;
+  final String atividadesDesenvolvidas;
+  final String tecnologias;
+  final String feitosDestaque;
 
   TimeLine(
       {@required this.date,
       @required this.name,
       @required this.iconAsset,
+      this.atividadesDesenvolvidas,
+      this.tecnologias,
+      this.feitosDestaque,
       @required this.subtitle});
 }
 
@@ -25,22 +31,45 @@ class TimeLineRepository {
           date: '2016-2017',
           name: 'Mirante Tecnologia',
           iconAsset: 'mirante.png',
-          subtitle: 'Estágiario'),
+          subtitle: 'Estágiario',
+          feitosDestaque: '''Integração com VOIP(Callbox) via API REST.
+Inclusão do Flyway para o gerenciamento de scripts.''',
+          tecnologias:
+              'Java 8, Spring, Wicket, HTML, CSS, JS, JasperReport, Git e SQL Server.',
+          atividadesDesenvolvidas:
+              '''Criação e manutenção de um sistema para gerenciamento de hotéis, contratos, e área
+de atendimento para o contratante do serviço prestado pela Montreal Turismo.'''),
       TimeLine(
           date: '2017-2019',
           name: 'Singular',
           iconAsset: 'singular.png',
-          subtitle: 'Engenheiro de Software'),
+          subtitle: 'Engenheiro de Software',
+          tecnologias: 'Java 8, Hibernate, Wicket, Spring e Git.',
+          atividadesDesenvolvidas:
+              '''Desenvolvimento de componentes e manutenção na arquitetura para uma plataforma
+open source.'''),
       TimeLine(
           date: '2019-2020',
           name: 'Mirante Tecnologia',
           iconAsset: 'mirante.png',
-          subtitle: 'Engenheiro de Software'),
+          subtitle: 'Engenheiro de Software',
+          tecnologias: 'Flutter, Java 8, Java 11, Angular, Angular JS, JSF, Hibernate, Wicket, Spring, Jenkins, Gitlab CI/CD, OracleSQL e SQLServer. ',
+          feitosDestaque: '''Realização de integração com API REST para realização de pagamentos com PagTesouro.
+Evolução e manutenção de um dos principais sistemas da Anvisa.''',
+          atividadesDesenvolvidas:
+              '''Análise e desenvolvimento de sistemas para a Anvisa, que proporcionou uma economia de mais de 32 milhões em um ano.
+Análise e desenvolvimento de um aplicativo Mobile para o Incra e de uma API REST para comunicação.'''),
       TimeLine(
           date: '2020-2020',
           name: 'Digital Innovation One',
           iconAsset: 'digital_innovation_one.png',
-          subtitle: 'Instrutor de Flutter'),
+          subtitle: 'Instrutor de Flutter',
+          tecnologias: 'Flutter, Gitlab CI. ',
+          atividadesDesenvolvidas:
+              '''Criação de 3 módulos ensinando Flutter.
+Um módulo introdutório.
+Um módulo contendo integração com API Rest.
+Um módulo ensinando a realizar uma aplicação do zero à produção.'''),
       TimeLine(
           date: '2020',
           name: 'Instituto de Gestão e Tecnologia da Informação',
@@ -50,7 +79,13 @@ class TimeLineRepository {
           date: '2020',
           name: 'Singular Studio',
           iconAsset: 'singular.png',
-          subtitle: 'Engenheiro de Software'),
+          subtitle: 'Engenheiro de Software',
+          feitosDestaque: '''Integração de uma ferramente para assinatura digital.
+Integração de login com o GOV.BR via OAUTH2.
+Inclusão do Flyway para o gerenciamento de scripts.''',
+          tecnologias: 'Java 8, Hibernate, Wicket, Spring, Git e PostgreSQL.',
+          atividadesDesenvolvidas:
+              '''Criação, implementação, manutenção e implantação de uma plataforma no code.'''),
     ];
   }
 }
--- lib/timeline/timeline.dart
+++ lib/timeline/timeline.dart
@@ -4,7 +4,37 @@ import 'package:my_page/shared/constans.dart';
 import 'package:my_page/timeline/time_line_repository.dart';
 import 'package:timelines/timelines.dart';
 
-class TimeLineWidget extends StatelessWidget {
+class TimeLineWidget extends StatefulWidget {
+  @override
+  _TimeLineWidgetState createState() => _TimeLineWidgetState();
+}
+
+class _TimeLineWidgetState extends State<TimeLineWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animatable<Color> background;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 8),
+      vsync: this,
+    )..repeat();
+
+    background = TweenSequence<Color>(
+      [
+        TweenSequenceItem(
+          weight: 1.0,
+          tween: ColorTween(
+            begin: Colors.white10.withOpacity(0.2),
+            end: Colors.black12.withOpacity(0.1),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var timeLines = TimeLineRepository.getTimeLines();
@@ -31,43 +61,151 @@ class TimeLineWidget extends StatelessWidget {
             itemCount: timeLines.length,
             indicatorBuilder: (_, index) {
               var timeline = timeLines[index];
-              return Container(
-                decoration: new BoxDecoration(
-                    shape: BoxShape.circle, boxShadow: [kDefaultStrongShadow]),
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('timeline/${timeline.iconAsset}'),
-                  maxRadius: 50.0,
-                ),
-              );
+              return _buildImage(timeline);
             }),
       ),
     );
   }
 
-  Card _buildCard(TimeLine timeline, BuildContext context) {
-    return Card(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
+  Container _buildImage(TimeLine timeline) {
+    return Container(
+      decoration: new BoxDecoration(
+          shape: BoxShape.circle, boxShadow: [kDefaultStrongShadow]),
+      child: CircleAvatar(
+        backgroundImage: AssetImage('timeline/${timeline.iconAsset}'),
+        maxRadius: 50.0,
+      ),
+    );
+  }
+
+  Widget _buildCard(TimeLine timeline, BuildContext context) {
+    return AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return InkWell(
+            onTap: timeline.atividadesDesenvolvidas != null
+                ? _showDialog(timeline)
+                : null,
+            child: Card(
+              color: timeline.atividadesDesenvolvidas != null
+                  ? background
+                      .evaluate(AlwaysStoppedAnimation(_controller.value))
+                  : Colors.white10.withOpacity(0.2),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        timeline.date,
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic, fontSize: 20.0),
+                      ),
+                      Text(
+                        timeline.name,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      Text(
+                        timeline.subtitle,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _infoAtividade(String title) {
+    return Text(
+      title,
+      style: Theme.of(context).textTheme.headline6,
+    );
+  }
+
+  _showDialog(TimeLine timeline) {
+    return () => showDialog(
+        context: this.context,
+        builder: (_) => Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              child: contentBox(timeline, context),
+            ));
+  }
+
+  contentBox(TimeLine timeline, BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: 800,
+          padding: EdgeInsets.only(left: 15.0, right: 10.0, top: 40.0),
+          margin: EdgeInsets.only(top: 75.0),
+          decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [kDefaultStrongShadow]),
           child: Column(
-            children: [
-              Text(
-                timeline.date,
-                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0),
-              ),
-              Text(
-                timeline.name,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              Text(
-                timeline.subtitle,
-                style: Theme.of(context).textTheme.headline6,
-              ),
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(timeline.name, style: Theme.of(context).textTheme.headline4),
+              SizedBox(height: 15),
+              _infoAtividade('Atividades Desenvolvidas:'),
+              _infoDetailAtividade(timeline.atividadesDesenvolvidas),
+              SizedBox(height: 10),
+              _infoAtividade('Tecnologias:'),
+              _infoDetailAtividade(timeline.tecnologias),
+              SizedBox(height: 10),
+              Visibility(
+                  visible: timeline.feitosDestaque != null,
+                  child: _infoAtividade('Feitos de destaque:')),
+              _infoDetailAtividade(timeline.feitosDestaque),
+              SizedBox(height: 10),
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: FlatButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text(
+                          'fechar',
+                          style: TextStyle(fontSize: 18, color: Colors.blue),
+                        )),
+                  )),
             ],
           ),
         ),
-      ),
+        Positioned(
+            left: 10,
+            right: 10,
+            child: CircleAvatar(
+              maxRadius: 50.0,
+              backgroundColor: Colors.transparent,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(60)),
+                  child: Image.asset(
+                    'timeline/${timeline.iconAsset}',
+                  )),
+            )),
+      ],
     );
+  }
+
+  Padding _infoDetailAtividade(String detailAtividade) {
+    return Padding(
+        padding: EdgeInsets.only(
+          top: 5.0,
+          bottom: 15.0,
+        ),
+        child: Text(
+          detailAtividade ?? '',
+          style: Theme.of(context).textTheme.subtitle1,
+        ));
   }
 }
